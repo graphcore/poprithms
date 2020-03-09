ed51de5fa2e5adb31029d5e87736763da551289a
James Newling <jamesn@graphcore.ai>
2020-03-09 11:45:55 +0000

    Faster (smaller) boost test framework include path
    Summary:
    Before this diff, the include path for the boost smoke test,
    smoke_test_boost_unit_test_framework.cpp was,
    #include <boost/test/included/unit_test.hpp>
    
    This test was taking 10x longer to compile than the other tests. Some digging,
    and I found that changing the include path to,
    #include <boost/test/unit_test.hpp>
    
    result in a 5x speed-up. Note that this path does not work with boost
    1.72 on OS/X
    
    Some compile times post this diff:
    - small boost-less file (alloc.cpp) 0m0.448s
    - large boost-less file (graph.cpp) 0m1.264s
    - small with-BOOST file (dfs.cpp)   0m1.870s
    - small boost-less test, build      0m0.595s
    - small boost-less test, link       0m0.016s
    - small with-BOOST test, build      0m1.077s (was 5 seconds before this diff)
    
    Conclusion: boost graph inclusion incurs 1.5 seconds,
    using boost test framework incurs 0.5 seconds.
    
    All this on OS/X, may be different with different compilers, boosts, OSs, etc.
    
    Test Plan: Test compilation faster, test still passes
    
    Reviewers: #poprithms, davidn
    
    Reviewed By: #poprithms, davidn
    
    Subscribers: romand, georgep, jakeh, stephenm
    
    Maniphest Tasks: T16838
    
    Differential Revision: https://phabricator.sourcevertex.net/D22322

--- poprithms/tests/tests/boostsmoke/smoke_test_boost_unit_test_framework.cpp
+++ poprithms/tests/tests/boostsmoke/smoke_test_boost_unit_test_framework.cpp
@@ -1,14 +1,8 @@
 #define BOOST_TEST_MODULE boostsmoke_smoke_test_boost_unit_test_framework_cpp
 
-// Note: the include directory must be like this for recent versions of
-// boost
+// Using this include path is 5x slower for compilation
 // #include <boost/test/included/unit_test.hpp>
-// Previously (and in popart this somehow still works) this worked
-// #include <boost/test/unit_test.hpp>
-// Solution discovered here:
-// https://stackoverflow.com/questions/33644088/linker-error-while-building-unit-tests-with-boost
-
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include <vector>
 #include <poprithms/schedule/anneal/graph.hpp>
 
