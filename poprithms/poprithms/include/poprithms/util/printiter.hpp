75ecb94164b346f28dfcde5a015401ba9e80f04a
James Newling <jamesn@graphcore.ai>
2020-05-14 17:51:23 +0100

    Expose template to print std::vector
    Summary: Fixes T20740
    
    Reviewers: #poprithms, fabiant
    
    Reviewed By: #poprithms, fabiant
    
    Maniphest Tasks: T20740
    
    Differential Revision: https://phabricator.sourcevertex.net/D25943

--- poprithms/poprithms/include/poprithms/util/printiter.hpp
+++ poprithms/poprithms/include/poprithms/util/printiter.hpp
@@ -7,9 +7,28 @@
 namespace poprithms {
 namespace util {
 
-void append(std::ostream &, const std::vector<int64_t> &);
-void append(std::ostream &, const std::vector<int> &);
-void append(std::ostream &, const std::vector<uint64_t> &);
+// If t = {1,2,4,5}, appends "(1,2,4,5)" to os.
+template <typename T> void append(std::ostream &os, const std::vector<T> &t) {
+  os << '(';
+  if (t.size() > 0) {
+    // note that std::cbegin is only introduced in C++14, so using std::begin
+    // to ensure C++11 API.
+    auto t0 = std::begin(t);
+    os << *t0;
+    ++t0;
+    while (t0 != std::end(t)) {
+      os << ',' << *t0;
+      ++t0;
+    }
+  }
+  os << ')';
+}
+
+extern template void append<>(std::ostream &, const std::vector<int64_t> &);
+extern template void append<>(std::ostream &, const std::vector<uint64_t> &);
+extern template void append<>(std::ostream &, const std::vector<int> &);
+extern template void append<>(std::ostream &,
+                              const std::vector<std::string> &);
 
 } // namespace util
 } // namespace poprithms
--- poprithms/poprithms/src/poprithms/util/printiter.cpp
+++ poprithms/poprithms/src/poprithms/util/printiter.cpp
@@ -1,42 +1,15 @@
-#include <poprithms/schedule/anneal/error.hpp>
+#include <initializer_list>
+#include <string>
+
 #include <poprithms/util/printiter.hpp>
 
 namespace poprithms {
 namespace util {
-namespace {
 
-template <typename T, typename V>
-void tvAppend(std::ostream &os, const T &t, const V &v) {
-  if (t.size() > 0) {
-    auto t0 = std::cbegin(t);
-    os << v(*t0);
-    ++t0;
-    while (t0 != std::cend(t)) {
-      os << ',' << v(*t0);
-      ++t0;
-    }
-  }
-}
-
-template <typename T> class Id {
-public:
-  T operator()(const T &t) const { return t; }
-};
-
-template <typename T> void tAppend(std::ostream &os, const T &t) {
-  using E = typename T::value_type;
-  tvAppend(os, t, Id<E>());
-}
-
-} // namespace
-
-void append(std::ostream &os, const std::vector<uint64_t> &v) {
-  tAppend(os, v);
-}
-void append(std::ostream &os, const std::vector<int64_t> &v) {
-  tAppend(os, v);
-}
-void append(std::ostream &os, const std::vector<int> &v) { tAppend(os, v); }
+template void append<>(std::ostream &, const std::vector<int64_t> &);
+template void append<>(std::ostream &, const std::vector<uint64_t> &);
+template void append<>(std::ostream &, const std::vector<int> &);
+template void append<>(std::ostream &, const std::vector<std::string> &);
 
 } // namespace util
 } // namespace poprithms
