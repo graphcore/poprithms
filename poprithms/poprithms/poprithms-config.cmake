1cb95499b69c98c327be15f06cef1b39682e9d4b
Sam Loescher <saml@graphcore.ai>
2020-05-18 17:59:01 +0100

    <<Replace this line with your revision title>
    Summary: This reverts commit 111aaa5fd4b96bc2c0d0246aa40fdc45694c0025.
    
    Test Plan: revert-hammer
    
    Reviewers:
    
    Subscribers:

--- poprithms/poprithms/poprithms-config.cmake
+++
@@ -1,7 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-get_filename_component(poprithms_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-
-if (NOT TARGET poprithms)
-  include("${poprithms_CMAKE_DIR}/poprithms-targets.cmake")
-endif()
