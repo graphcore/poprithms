ff28d02942f970c06a12c0f91a219f649b01c6f0
Oskar Bunyan <oskarb@graphcore.ai>
2020-08-14 19:03:53 +0100

    Add another bin constraint test
    Reviewers: #poprithms, jamesn
    
    Reviewed By: #poprithms, jamesn
    
    Subscribers: jamesn
    
    Maniphest Tasks: T25330
    
    Differential Revision: https://phabricator.sourcevertex.net/D30586

--- poprithms/tests/tests/schedule/anneal/bin_constraints.cpp
+++ poprithms/tests/tests/schedule/anneal/bin_constraints.cpp
@@ -4,39 +4,60 @@
 #include <poprithms/schedule/anneal/error.hpp>
 #include <poprithms/schedule/anneal/graph.hpp>
 
-int main() {
-
+void skipping_bin_test() {
   using namespace poprithms::schedule::anneal;
 
   Graph g;
-  auto op0 = g.insertOp("op0");
-  auto op1 = g.insertOp("op1");
-  auto op2 = g.insertOp("op2");
-
-  auto alloc = g.insertAlloc(100);
-  g.insertOpAlloc(op0, alloc);
-  g.insertOpAlloc(op2, alloc);
-
-  g.insertConstraint(op0, op1);
-  g.insertConstraint(op0, op2);
+  const auto ops  = g.insertOps({"op0", "op1", "op2"});
+  const auto allo = g.insertAlloc(100);
+  g.insertOpAlloc({ops[0], ops[1]}, allo);
+  g.insertConstraints({{ops[0], ops[1]}, {ops[0], ops[2]}});
 
   // Due to alloc liveness, preferred schedule would be:
   //    op0,op2,op1
   // However, the following bin constraints should force the schedule to be:
   //    op0,op1,op2
 
-  std::vector<std::vector<OpAddress>> bins(3);
-  bins[0].push_back(op0);
-  bins[0].push_back(op1);
-  bins[2].push_back(op2);
-
-  g.insertBinConstraints(bins, "test");
+  g.insertBinConstraints({{ops[0], ops[1]}, {}, {ops[2]}}, "test");
 
   g.initialize();
   g.minSumLivenessAnneal();
-  auto op1_before_op2 = g.opToSchedule(op1) < g.opToSchedule(op2);
+  auto op1_before_op2 = g.opToSchedule(ops[1]) < g.opToSchedule(ops[2]);
   if (!op1_before_op2) {
-    throw error("Bin constraints should force op1  to be before op2");
+    throw error("Skipping bin constraints should force op1 to be before op2");
   }
+}
+
+void multiple_bin_test() {
+  using namespace poprithms::schedule::anneal;
+
+  Graph g;
+  const auto ops  = g.insertOps({"op0", "op1", "op2"});
+  const auto allo = g.insertAlloc(100);
+  g.insertOpAlloc({ops[0], ops[1]}, allo);
+  g.insertConstraints({{ops[0], ops[1]}, {ops[0], ops[2]}});
+
+  // Due to alloc liveness, preferred schedule would be:
+  //    op0,op2,op1
+  // However, the following nested bin constraints should force the schedule
+  // to be:
+  //    op0,op1,op2
+
+  g.insertBinConstraints({{ops[0]}, {ops[1]}}, "phases");
+  g.insertBinConstraints({{ops[0], ops[1]}, {ops[2]}}, "context");
+
+  g.initialize();
+  g.minSumLivenessAnneal();
+  auto op1_before_op2 = g.opToSchedule(ops[1]) < g.opToSchedule(ops[2]);
+  if (!op1_before_op2) {
+    throw error("Multiple bin constraints should force op1 to be before op2");
+  }
+}
+
+int main() {
+  skipping_bin_test();
+
+  multiple_bin_test();
+
   return 0;
 }
