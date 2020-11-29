67166b95777dfc332a59d4c471bfbdb037d0b5fe
James Newling <jamesn@graphcore.ai>
2020-11-29 20:47:39 +0000

    Remove ununsed bazel and static-config files
    Summary:
    Now that poprithms and packaged in the same was as poplar
    and poplibs, other frameworks should use it in the same way.
    
    Reviewers: #poprithms, evan
    
    Reviewed By: #poprithms, evan
    
    Maniphest Tasks: T31228
    
    Differential Revision: https://phabricator.sourcevertex.net/D36957

--- poprithms/poprithms/poprithms-static-config.cmake
+++
@@ -1,9 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-get_filename_component(poprithms_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-
-find_dependency(Threads REQUIRED)
-
-if (NOT TARGET poprithms-static)
-  include("${poprithms_CMAKE_DIR}/poprithms-static-targets.cmake")
-endif()
