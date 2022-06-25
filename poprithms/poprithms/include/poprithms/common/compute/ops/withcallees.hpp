bbff267a06f7415bd704652e86a35722e2e664a6
James Newling <jamesn@graphcore.ai>
2022-06-25 09:42:09 +0100

    Base class for ops with callees (call, switch, repeat, etc.)
    Test Plan: Tests will arrive with the non-abstract classes in later diffs.
    
    Reviewers: shirazb, #poprithms, romand
    
    Reviewed By: #poprithms, romand
    
    Subscribers: romand
    
    Maniphest Tasks: T64048
    
    Differential Revision: https://phabricator.sourcevertex.net/D69955

---
+++ poprithms/poprithms/include/poprithms/common/compute/ops/withcallees.hpp
@@ -0,0 +1,321 @@
+// Copyright (c) 2022 Graphcore Ltd. All rights reserved.
+#ifndef POPRITHMS_COMMON_COMPUTE_OPS_WITHCALLEES_HPP
+#define POPRITHMS_COMMON_COMPUTE_OPS_WITHCALLEES_HPP
+
+#include <poprithms/common/compute/ihostrunner.hpp>
+#include <poprithms/common/compute/op.hpp>
+#include <poprithms/program/callstack/copyin.hpp>
+#include <poprithms/program/callstack/copyout.hpp>
+
+namespace poprithms {
+namespace common {
+namespace compute {
+
+using poprithms::common::multiout::InIndex;
+using poprithms::common::multiout::InIndices;
+using poprithms::program::callstack::CopyIn;
+using poprithms::program::callstack::CopyOuts;
+
+/**
+ * The base class for ops such as Call, Repeat, Switch, etc -- all the ops
+ * which have callee sub-graphs.
+ * */
+class WithCallees : public Op {
+
+private:
+  // The sub-graphs which this op calls. For call/repeat this will just be 1
+  // sub-graph id, for switch it will be multiple.
+  SubGraphIds callees_;
+
+  // The destinations of the copies into the callees.
+  CalleeTensorIds inDsts_;
+
+  // The sources of the copies out of the callees.
+  CopyOuts outs_;
+
+public:
+  /**
+   * \param opState The input ids, input types, output types, etc.
+   *
+   * \param callees The sug-graph(s) to call. There must be at least one
+   *                sub-graph (this vector cannot be empty, ops with no
+   *                callees can inherit from WithoutCallees).
+   *
+   * \param inDsts The input copy destinations.
+   *
+   * \param outs The sources of the copies out of the callees, back into the
+   *             calling scope.
+   *
+   * Recursive calls to sub-graphs are not supported. For example an op cannot
+   * have its own sub-graph as one of its callee.
+   *
+   * This op has some inputs which are copied to sub-graphs, and some which
+   * are not. All the copy inputs must appear at input indices lower than the
+   * non-copy inputs.
+   *
+   * Currently this op assumes that all outputs are copied from sub-graphs.
+   * */
+  WithCallees(const State &opState,
+              const SubGraphIds &callees,
+              const CalleeTensorIds &inDsts,
+              const CopyOuts &outs);
+
+  /**
+   * This op has some inputs which are copied to callee sub-graphs, and might
+   * have some inputs which are not. This method returns the indices of inputs
+   * which are not copied to callee sub-graphs. An example of such an input
+   * would be the conditional tensor of a switch op.
+   *
+   * This class assumes that all of these indices appear contiguously after
+   * all indices of copy inputs.
+   * */
+  InIndices nonCopyToCalleeIndices() const;
+
+  /**
+   * All of the inputs which are copied to callee sub-graphs. This is all
+   * input indices not in #nonCopyToCalleeIndices.
+   *
+   * This class assumes that all of these indices appear contiguously before
+   * all indices of non-copy inputs.
+   * */
+  InIndices calleeCopyInIndices() const;
+
+  /**
+   * \return The number of inputs which are not copied to callee sub-graphs.
+   * */
+  virtual uint64_t nNonCopyIns() const = 0;
+
+  /**
+   * \return The number of inputs which are copied to callee sub-graphs.
+   * */
+  uint64_t nCopyIns() const { return nInTensors() - nNonCopyIns(); }
+
+  /**
+   * All the input copies are at input indices lower than the non-copy inputs.
+   * */
+  bool isCopyToCalleeInIndex(InIndex i) const final {
+    return i.get() < nCopyIns();
+  }
+
+  /**
+   * The destinations in the callee sub-graphs of the input copies. The size
+   * of the returned vector is the same as that returned by
+   * #calleeCopyInIndices, and there is a 1:1 correspondence.
+   * */
+  const CalleeTensorIds &inDsts() const { return inDsts_; }
+
+  /**
+   * The destinations in the callee sub-graphs. This is the same as the vector
+   * returned by #inDsts, but with the callee sub-graph indices removed.
+   * */
+  TensorIds inTensorIdDsts() const;
+
+  /**
+   * The destination of the #i'th input copy.
+   *
+   * \param i An input index, must be less than #nCopyIns.
+   * */
+  CalleeTensorId dstInCallee(InIndex i) const final;
+
+  /**
+   * The number of inputs which are copied to callee sub-graphs.
+   * */
+  uint64_t nInCopies() const { return inDsts_.size(); }
+
+  /**
+   * The copy destinations in callee sub-graphs of the inputs at indices
+   * #inds.
+   * */
+  TensorIds inDsts(const InIndices &inds) const;
+
+  /**
+   * The sources of all the copies into callee sub-graph #ci.
+   * */
+  TensorIds inSrcs(CalleeIndex ci) const;
+
+  /**
+   * The destinations of copies in the callee sub-graph #ci.
+   * */
+  TensorIds inDsts(CalleeIndex ci) const;
+
+  /**
+   * \return The CalleeTensorId #ctId is made up a tensor id (say #tId) and a
+   *         callee sub-graph index (say #ci). This method returns true if the
+   *         tensor #tId is the destination of a copy into the sub-graph
+   *         #ci, from a tensor in this op's sub-graph.
+   **/
+  bool isDstInCallee(const CalleeTensorId &ctId) const final;
+
+  /**
+   * \return true if the callee sub-graph tensor #ctId is copied out from,
+   *         when this op completes.
+   * */
+  bool isSrcInCallee(const CalleeTensorId &ctId) const final;
+
+  /**
+   * \return The tensor in the callee sub-graph #ci which is copied from at
+   *         output index #o.
+   *
+   * Note: the current design of the op class assumes that all of the outputs
+   * of an op with callees are copied from callee sub-graphs. This is
+   * different to inputs, where for example the switch op has a #condition
+   * input which is not copied to a sub-graph.
+   * */
+  TensorId srcInCallee(OutIndex o, CalleeIndex ci) const final;
+
+  /**
+   * The CalleeTensorId #ctId is made up of a tensor in this op's sub-graph,
+   * say #tId, and a callee sub-graph index, say #ci.
+   *
+   * \return The tensors in the callee sub-graph #ci to which the tensor #tId
+   *         is copied.
+   * */
+  TensorIds dstsInCallee(const CalleeTensorId &ctId) const final;
+
+  /**
+   * \return The input index at which the callee sub-graph tensor #ctId is
+   *         copied to at.
+   * */
+  InIndex inIndex(const CalleeTensorId &ctId) const final;
+
+  /**
+   * \return true if the output at index #o is copied out of callee sub-graph
+   *         #ci.
+   * */
+  bool isCopiedOut(OutIndex o, CalleeIndex ci) const final;
+
+  /**
+   * \return The output index at which the callee sub-graph tensor #ctId is
+   *         copied out of this op.
+   * */
+  OutIndex outIndex(const CalleeTensorId &) const final;
+
+  void runSim(SimTensorMap &) const final;
+
+  /**
+   * Run this op on cpu.
+   * */
+  virtual void hostRun(const IHostRunner &) const = 0;
+
+  CallEvent event(CalleeIndex i) const {
+    return CallEvent(id(), callee(i), i);
+  }
+
+  /**
+   * The total number of callee sub-graphs of this op. For ops such as Call
+   * and Repeat this will be 1, for others like Switch it is more than 1.
+   * */
+  uint64_t nCallees() const final { return callees_.size(); }
+
+  /**
+   * \return true if #sgId is one of this op's callee sub-graphs.
+   * */
+  bool isCallee(SubGraphId) const;
+
+  SubGraphIds callees() const final { return callees_; }
+
+  SubGraphId callee(CalleeIndex i) const final {
+    return callees_.at(i.get());
+  }
+
+  /**
+   * Zip the input copy sources and destinations together, and return. \sa
+   * CopyIn.
+   * */
+  std::vector<CopyIn> copyIns() const;
+
+  const CopyOuts &outs() const { return outs_; }
+
+  void appendWithCalleesAttributes(std::ostream &) const;
+
+  /**
+   * Given that gradients are required for the inputs #fromTargets and
+   * gradients for the outputs at indices #inGrads are provided, determine the
+   * autodiff objective.
+   * */
+  virtual poprithms::autodiff::guide::Objective
+  localObjective(CalleeIndex,
+                 const InIndices &fromTargets,
+                 const OutIndices &inGrads) const = 0;
+
+  /**
+   * \return true if this op calls a callee multiple times, and the tensor
+   *         #tId is a loop carry dependency. Specifically, return true if
+   *         #tId is copied to at the end of each iteration, from another
+   *         tensor in the callee sub-graph.
+   * */
+  virtual bool isCarriedTo(const TensorId &tId) const = 0;
+
+  void computeDerivedVerifyValid() const final;
+
+private:
+  /**
+   * Only RefFrom_ can have an output which references a tensor in a different
+   * sub-graph.
+   * */
+  TensorId rootRef(OutIndex o) const final { return outTensorId(o); }
+  void resetRootRef(OutIndex, const TensorId &) { invalid(); }
+
+  /**
+   * Reset the destination of an in-copy.
+   * */
+  void resetCalleeTensorId(InIndex, const CalleeTensorId &newId) final;
+
+  /**
+   * Reset the source (in a callee sub-graph) of an out-copy.
+   * */
+  void resetOutSource(OutIndex,
+                      CalleeIndex,
+                      const TensorId &newSourceInCallee) final;
+
+  void growAliasMapper(MemoryAliasMapper &mam) const final {
+    return createVariables(mam);
+  }
+
+  /**
+   * Ops with callees do execute code at runtime (unlike say the 'VarInit' and
+   * 'Reshape_' ops which do not.
+   * */
+  bool isInitializingOp() const final { return false; }
+
+  bool computeTypeSpecificEqualTo(const Op &) const final;
+
+  /**
+   * If all ops in all callees have CodeLocation::Ipu, then this calling op
+   * has CodeLocation::Ipu. Otherwise, it has CodeLocation::Host.
+   * */
+  CodeLocation codeLocation() const final;
+
+  /**
+   * Append the relevant callee checkpoint tensors to #ids.
+   * */
+  void extendAutodiffRequiredTensors(
+      poprithms::autodiff::automatic::RequiredIds &ids) const final;
+
+  virtual bool withCalleesTypeSpecificEqualTo(const Op &) const = 0;
+
+  virtual void withCalleesTypeSpecificAssertValid() const = 0;
+
+  void computeDerivedRemoveInputs(const ContiguousInIndexSubset &) final;
+  virtual void
+  withCalleesDerivedRemoveInputs(const ContiguousInIndexSubset &) = 0;
+
+  void computeDerivedRemoveOutputs(const ContiguousOutIndexSubset &) final;
+  virtual void
+  withCalleesDerivedRemoveOutputs(const ContiguousOutIndexSubset &) = 0;
+
+  /**
+   * All outputs are new allocations.
+   * */
+  HostTensors initializeOut(const HostTensors &) const final;
+
+  void initializeSimOut(SimTensorMap &htm) const final {
+    initializeReplicatedSimOut(htm);
+  }
+};
+
+} // namespace compute
+} // namespace common
+} // namespace poprithms
+
+#endif
---
+++ poprithms/poprithms/src/poprithms/common/compute/ops/withcallees.cpp
@@ -0,0 +1,435 @@
+// Copyright (c) 2022 Graphcore Ltd. All rights reserved.
+
+#include <sstream>
+
+#include <common/compute/error.hpp>
+
+#include <poprithms/common/compute/graph.hpp>
+#include <poprithms/common/compute/ops/withcallees.hpp>
+#include <poprithms/common/compute/opverifier.hpp>
+
+namespace poprithms {
+namespace common {
+namespace compute {
+
+using poprithms::program::callstack::CopyIns;
+
+bool WithCallees::isCallee(SubGraphId sgId) const {
+  return std::find(callees_.cbegin(), callees_.cend(), sgId) !=
+         callees_.cend();
+}
+
+void WithCallees::computeDerivedVerifyValid() const {
+
+  auto base = [this]() {
+    std::ostringstream oss;
+    appendWithCalleesAttributes(oss);
+    return oss.str();
+  };
+
+  if (callees_.empty()) {
+    throw error(base() + " Callees cannot be empty in WithCallees.");
+  }
+
+  // While this doesn't catch all recursion, it catches one of the common
+  // mistakes (sub-graph calls itself).
+  if (std::find(callees_.cbegin(), callees_.cend(), subGraphId()) !=
+      callees_.cend()) {
+    std::ostringstream oss;
+    oss << base() + " Recursion not supported, but caller (" << subGraphId()
+        << ") is in the list of callees (" << callees_ << "). ";
+    throw error(oss.str());
+  }
+
+  // Check that the sources of the outputs are in the correct sub-graph.
+  for (CalleeIndex ci = 0; ci < nCallees(); ++ci) {
+    for (OutIndex oi = 0; oi < nOutTensors(); ++oi) {
+      if (outs().hasValue(oi, ci)) {
+        auto o = outs().outSource(oi, ci);
+        if (computeGraph().subGraphId(o) != callee(ci)) {
+          std::ostringstream oss;
+          oss << base() + " Invalid copy out, " << o
+              << " is not in sub-graph " << callee(ci)
+              << ", it is in sub-graph " << computeGraph().subGraphId(o);
+          throw error(oss.str());
+        }
+      }
+    }
+  }
+
+  for (InIndex i = 0; i < inDsts().size(); ++i) {
+    if (inDsts_.at(i.get()).calleeIndex() >= callees_.size()) {
+      throw error(base() + " Invalid callee index, it's too large.");
+    }
+  }
+
+  for (InIndex i = 0; i < inDsts().size(); ++i) {
+    const auto ci = dstInCallee(i);
+    if (computeGraph().subGraphId(ci.tId()) !=
+        callees_.at(ci.calleeIndex().get())) {
+      throw error(base() +
+                  " Disagreement about sub-graph of copy in destination.");
+    }
+  }
+
+  for (InIndex i = 0; i < inDsts().size(); ++i) {
+    const auto ci = dstInCallee(i);
+    if (computeGraph().dtype(ci.tId()) !=
+        computeGraph().dtype(inTensorId(i))) {
+      std::ostringstream oss;
+      oss << base() << " Type disagreement at input #i.";
+      throw error(oss.str());
+    }
+  }
+
+  if (outs().nOutTensors() != nOutTensors()) {
+    throw error(base() + " Disagreement about number of outputs (currently "
+                         "assuming all outputs are copies).");
+  }
+
+  if (outs().nOutTensors() > 0) {
+    if (outs().nCallees() != callees_.size()) {
+      std::ostringstream oss;
+      oss << base() << " Number of callees according to outs is "
+          << outs().nCallees() << ", but number of callees in callees_ is "
+          << callees_.size();
+      throw error(oss.str());
+    }
+
+    for (OutIndex o = 0; o < nOutTensors(); ++o) {
+      for (CalleeIndex c = 0; c < nCallees(); ++c) {
+        if (outs().hasValue(o, c)) {
+          auto tId = outs().outSource(o, c);
+          if (computeGraph().subGraphId(tId) != callee(c)) {
+            throw error(base() +
+                        " Disagreement about sub-graph of copy out source.");
+          }
+
+          if (computeGraph().dtype(tId) != outDType(o)) {
+            std::ostringstream oss;
+            oss << base() << " Disagreement between source and "
+                << "destination of out copy tensor info.";
+            oss << "source has info " << computeGraph().tensorInfo(tId)
+                << " and destination has info " << outTensorInfo(o) << '.';
+            throw error(oss.str());
+          }
+        }
+      }
+    }
+  }
+
+  OpVerifier(*this).verifyFromAtts({OpVerifier::Att::SameDeviceType});
+
+  for (CalleeIndex ci = 0; ci < nCallees(); ++ci) {
+    std::set<TensorId> outsAtIndex;
+    for (OutIndex oi = 0; oi < nOutTensors(); ++oi) {
+      if (outs().hasValue(oi, ci)) {
+        if (outsAtIndex.count(outs().outSource(oi, ci)) != 0) {
+          std::ostringstream oss;
+          oss << "Output sources must all be distinct at callee index " << ci
+              << ". Repeated outputs should be managed with copies/aliases. "
+              << "Output sources of " << *this << " for callee index " << ci
+              << " are " << outs().outSources(ci);
+          throw error(oss.str());
+        }
+        outsAtIndex.insert(outs().outSource(oi, ci));
+      }
+    }
+  }
+
+  for (InIndex i = 0; i < inDsts().size(); ++i) {
+    if (computeGraph().subGraphId(inTensorId(i).opId()) != subGraphId()) {
+      std::ostringstream oss;
+      oss << base() + " Invalid copy in at index " << i
+          << ", src sub-graph not valid for op " << *this;
+      throw error(oss.str());
+    }
+    if (!isCallee(computeGraph().subGraphId(dstInCallee(i).tId().opId()))) {
+      std::ostringstream oss;
+      oss << base() + " Invalid copy in at index " << i
+          << ", dst sub-graph not a callee.";
+      throw error(oss.str());
+    }
+  }
+
+  withCalleesTypeSpecificAssertValid();
+}
+
+void WithCallees::resetOutSource(OutIndex o,
+                                 CalleeIndex ci,
+                                 const TensorId &tId) {
+
+  for (OutIndex o_ = 0; o_ < nOutTensors(); ++o_) {
+    if (o != o_ && outs_.hasValue(o_, ci) && outs_.outSource(o_, ci) == tId) {
+      std::ostringstream oss;
+      oss << "For CalleeIndex " << ci
+          << " all the output tensors must be distinct "
+          << "at the output indices. "
+          << " But if " << tId << " is the new tensor at output index " << o
+          << " of " << *this
+          << " then it will be the output at at least 2 indices.";
+      throw error(oss.str());
+    }
+  }
+  outs_.reset(o, ci, tId);
+}
+
+InIndex WithCallees::inIndex(const CalleeTensorId &ctId) const {
+  for (InIndex i = 0; i < inDsts_.size(); ++i) {
+    if (inDsts_.at(i.get()) == ctId) {
+      return i;
+    }
+  }
+  throw error("callee tensor not present");
+}
+
+void WithCallees::resetCalleeTensorId(InIndex i, const CalleeTensorId &n) {
+  inDsts_.at(i.get()) = n;
+}
+
+CodeLocation WithCallees::codeLocation() const {
+
+  // Ensure that inputs and outputs all have same location.
+  auto deviceTypes = inDeviceTypes();
+  const auto y     = outDeviceTypes();
+  deviceTypes.insert(deviceTypes.end(), y.cbegin(), y.cend());
+
+  if (std::set<DeviceType>(deviceTypes.begin(), deviceTypes.end()).size() >
+      1) {
+    std::ostringstream oss;
+    oss << "WithCallees op " << *this
+        << ": all inputs and outputs must have same device type. "
+        << "DeviceTypes of inputs and outputs are ";
+    poprithms::util::append(oss, deviceTypes);
+  }
+
+  for (auto c : callees()) {
+    for (auto o : computeGraph().opIds(c)) {
+      if (computeGraph().computeOp(o).codeLocation() == CodeLocation::Host) {
+        return CodeLocation::Host;
+      }
+    }
+  }
+
+  return CodeLocation::Ipu;
+}
+
+void WithCallees::computeDerivedRemoveOutputs(
+    const ContiguousOutIndexSubset &coin) {
+  outs_.reduce(coin);
+  withCalleesDerivedRemoveOutputs(coin);
+}
+
+std::vector<CopyIn> WithCallees::copyIns() const {
+  auto ins_ = inTensorIds();
+  ins_      = {ins_.begin(), ins_.begin() + nInCopies()};
+  return CopyIns::zip(ins_, inDsts_);
+}
+
+bool WithCallees::computeTypeSpecificEqualTo(const Op &rhs) const {
+  const auto &rhs_ = static_cast<const WithCallees &>(rhs);
+  return callees_ == rhs_.callees() && subGraphId() == rhs_.subGraphId() &&
+         inDsts_ == rhs_.inDsts() && outs_ == rhs_.outs() &&
+         withCalleesTypeSpecificEqualTo(rhs);
+}
+
+HostTensors WithCallees::initializeOut(const HostTensors &) const {
+  return badValOuts();
+}
+
+CalleeTensorId WithCallees::dstInCallee(InIndex i) const {
+  if (i.get() >= nCopyIns()) {
+    std::ostringstream oss;
+    oss << "Invalid input index " << i << " in dstInCallee of op " << *this
+        << " which only has " << nCopyIns() << " input copies. ";
+    throw error(oss.str());
+  }
+  return inDsts_.at(i.get());
+}
+
+InIndices WithCallees::calleeCopyInIndices() const {
+  auto nons = nonCopyToCalleeIndices();
+  if (nons.empty()) {
+    return inIndices();
+  }
+  InIndices inIndices;
+  inIndices.reserve(nInTensors() - nons.size());
+  for (InIndex i = 0; i < nInTensors(); ++i) {
+    if (std::find(nons.cbegin(), nons.cend(), i) == nons.cend()) {
+      inIndices.push_back(i);
+    }
+  }
+  return inIndices;
+}
+
+void WithCallees::computeDerivedRemoveInputs(
+    const ContiguousInIndexSubset &coin) {
+
+  InIndices copyInIndices = calleeCopyInIndices();
+  coin.reduce(inDsts_, copyInIndices);
+  withCalleesDerivedRemoveInputs(coin);
+}
+
+void WithCallees::appendWithCalleesAttributes(std::ostream &ost) const {
+  ost << "WithCallees: "
+      << "\n      callee=" << callees() << "\n         out=" << outs();
+}
+
+bool WithCallees::isSrcInCallee(const CalleeTensorId &tId) const {
+  return outs_.isSource(tId.calleeIndex(), tId.tId());
+}
+
+TensorId WithCallees::srcInCallee(OutIndex o, CalleeIndex ci) const {
+  return outs_.outSource(o, ci);
+}
+
+bool WithCallees::isCopiedOut(OutIndex o, CalleeIndex ci) const {
+  return outs_.hasValue(o, ci);
+}
+
+void WithCallees::extendAutodiffRequiredTensors(
+    poprithms::autodiff::automatic::RequiredIds &ts) const {
+
+  // Some tensors in the callee graphs are copied out for use later in the
+  // gradient graph. These are the checkpoint tensors.
+  for (CalleeIndex calleeIndex = 0; calleeIndex < nCallees(); ++calleeIndex) {
+    const auto &gInf = ts.gradInfo(ts.grad(id(), calleeIndex));
+
+    // checkPointPair is a pair of tensors: 1 tensor in the callee graph and 1
+    // in the gradient graph. The one in the callee graph is copied out to a
+    // tensor #dst in the calling graph.
+    for (auto checkPointPair : gInf.checkpointPairs()) {
+      if (!outs().isSource(calleeIndex, checkPointPair.inNonGradGraph)) {
+        std::ostringstream oss;
+        oss << "Expected the tensor " << checkPointPair.inNonGradGraph
+            << " to be copied out of op " << *this
+            << ", as it is required as a checkpoint. "
+            << "In particular, the objective is " << gInf.objective() << '.';
+        throw error(oss.str());
+      }
+
+      const auto dst = computeGraph().dstInCaller(
+          checkPointPair.inNonGradGraph, event(calleeIndex));
+      ts.insert(dst);
+    }
+  }
+}
+
+void WithCallees::runSim(SimTensorMap &hts) const {
+  computeDerivedVerifyValid();
+  hostRun(SimHostRunner(hts, computeGraph()));
+}
+
+TensorIds WithCallees::inTensorIdDsts() const {
+  TensorIds ids_;
+  ids_.reserve(inDsts_.size());
+  for (const auto &inDst : inDsts_) {
+    ids_.push_back(inDst.tId());
+  }
+  return ids_;
+}
+
+OutIndex WithCallees::outIndex(const CalleeTensorId &x) const {
+  return outs_.outIndex(x.calleeIndex(), x.tId());
+}
+
+bool WithCallees::isDstInCallee(const CalleeTensorId &cId) const {
+  return std::find(inDsts_.cbegin(), inDsts_.cend(), cId) != inDsts_.cend();
+}
+TensorIds WithCallees::inDsts(const InIndices &inIndices) const {
+  TensorIds ids_;
+  ids_.reserve(inIndices.size());
+  for (auto i : inIndices) {
+    ids_.push_back(inDsts_.at(i.get()).tId());
+  }
+  return ids_;
+}
+
+TensorIds WithCallees::inDsts(CalleeIndex ci) const {
+  TensorIds ids_;
+  for (const auto &ctId : inDsts_) {
+    if (ctId.calleeIndex() == ci) {
+      ids_.push_back(ctId.tId());
+    }
+  }
+  return ids_;
+}
+
+TensorIds WithCallees::inSrcs(CalleeIndex ci) const {
+
+  TensorIds ids_;
+  for (InIndex i = 0; i < inDsts_.size(); ++i) {
+    if (inDsts_.at(i.get()).calleeIndex() == ci) {
+      ids_.push_back(inTensorId(i));
+    }
+  }
+  return ids_;
+}
+
+TensorIds WithCallees::dstsInCallee(const CalleeTensorId &inCaller) const {
+  TensorIds ids_;
+  for (InIndex i = 0; i < inDsts_.size(); ++i) {
+    if (inTensorId(i) == inCaller.tId()) {
+      auto x = inDsts_.at(i.get());
+      if (x.calleeIndex() == inCaller.calleeIndex()) {
+        ids_.push_back(x.tId());
+      }
+    }
+  }
+
+  return ids_;
+}
+
+WithCallees::WithCallees(const Op::State &opState,
+                         const SubGraphIds &callees,
+                         const CalleeTensorIds &inDsts,
+                         const CopyOuts &outs)
+    : Op(opState), callees_(callees), inDsts_(inDsts), outs_(outs) {}
+
+InIndices WithCallees::nonCopyToCalleeIndices() const {
+  InIndices inds;
+  inds.reserve(nNonCopyIns());
+  for (uint64_t i = nCopyIns(); i < nInTensors(); ++i) {
+    inds.push_back(InIndex(i));
+  }
+  return inds;
+}
+
+void IHostRunner::copies(const TensorIds &froms, const TensorIds &tos) const {
+
+  if (froms.size() != tos.size()) {
+    std::ostringstream oss;
+    oss << "froms is of size " << froms.size() << " and tos is of size "
+        << tos.size();
+    throw error(oss.str());
+  }
+  for (uint64_t inCopy = 0; inCopy < froms.size(); ++inCopy) {
+    copy(froms[inCopy], tos[inCopy]);
+  }
+}
+
+std::vector<HostTensors> IHostRunner::tensors(const TensorIds &tIds) const {
+  std::vector<HostTensors> ts;
+  ts.reserve(tIds.size());
+  for (auto tId : tIds) {
+    ts.push_back(tensor(tId));
+  }
+  return ts;
+}
+
+void IHostRunner::copy(const TensorId &from, const TensorId &to) const {
+
+  const auto tFrom = tensor(from);
+  const auto tTo   = tensor(to);
+  const auto rf    = tFrom.size();
+  if (rf != tTo.size()) {
+    throw error("Number of replicas for source and destination differ");
+  }
+  for (uint64_t replIndex = 0; replIndex < rf; ++replIndex) {
+    tTo[replIndex].update_(tFrom[replIndex]);
+  }
+}
+
+} // namespace compute
+} // namespace common
+} // namespace poprithms
